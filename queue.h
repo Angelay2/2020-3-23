0784ac4db4b335dd8d69db5b30eee5bc7a995266
Angelay2 <y2zaijian@126.com>
2020-04-07 23:55:55 +0800

    队列的头文件

--- queue.h
+++ queue.h
@@ -2,31 +2,31 @@
 #include <stdio.h>
 // 带有尾指针的单链表实现队列
 
-typedef int Datatype;
+typedef int QDatatype;
 // 定义结点
 typedef struct Node{
-	struct Node* _next;
-	Datatype _data;
-}Node;
+	struct QNode* _next;
+	QDatatype _data;
+}QNode;
 
 // 定义队列
 typedef struct Queue{
 	// 头尾指针
-	Node* _front;
-	Node* _rear;
+	QNode* _front;
+	QNode* _rear;
 	size_t _size;
 }Queue;
 // 函数
 // 初始化
 void queueInit(Queue* q);
 // 队尾插入
-void queuePush(Queue* q, Datatype data);
+void queuePush(Queue* q, QDatatype data);
 // 队头删除
 void queuePop(Queue* q);
 // 拿到队头元素
-Datatype queueFront(Queue* q);
+QDatatype queueFront(Queue* q);
 // 拿到队尾元素
-Datatype queueBack(Queue* q);
+QDatatype queueBack(Queue* q);
 // 判空
 int queueEmpty(Queue* q);
 // 输出队列大小
